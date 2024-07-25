f111327d3f185e7d5602e11928aa35d05b95ea3d
Getlar <tamastheactual@gmail.com>
2024-07-25 23:40:24 +0200

    fixed navbar, added tailwind config, removed colors, created new card: next

--- astro.config.ts
+++ astro.config.ts
@@ -1,20 +1,19 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
   // used to generate images
-  site:
-    process.env.VERCEL_ENV === 'production'
-      ? 'https://brutal.elian.codes/'
-      : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/`
-      : 'https://getlar.github.io',
+  site: process.env.VERCEL_ENV === 'production' ? 'https://brutal.elian.codes/' : process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/` : 'https://getlar.github.io',
   trailingSlash: 'ignore',
-  integrations: [sitemap(), UnoCSS({ injectReset: true })],
+  integrations: [sitemap(), UnoCSS({
+    injectReset: true
+  }), tailwind()],
   vite: {
     optimizeDeps: {
-      exclude: ['@resvg/resvg-js'],
-    },
-  },
+      exclude: ['@resvg/resvg-js']
+    }
+  }
 });
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+      colors: {
+        tcotta: '#89430a',
+        navbg: '#f4eee0',
+        bgc: '#edebe6',
+      },
+    },
+	},
+	plugins: [],
+};
